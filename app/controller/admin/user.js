f87d3f3e53d362bbb1843af6768f105c6d4c7f1d
dezhizhang <1541609448@qq.com>
2020-01-19 21:25:35 +0800

    修改

--- app/controller/admin/user.js
+++ app/controller/admin/user.js
@@ -1,31 +1,13 @@
 'use strict';
 
 const BaseController = require('./base');
-class ContactController extends BaseController {
+class UserController extends BaseController {
     async index() {
-        let { id } = this.ctx.query;
-        console.log(id);
-        // let result = await this.ctx.model.Contact.find();
-        // await this.ctx.render('/admin/contact/index',{
-        //     list:result
-        // });
+       console.log("1");
     }
-    //修改
-    async edit() {
-        let id = this.ctx.query.id;
-        let result = await this.ctx.model.Contact.find({'_id':id});
-        await this.ctx.render('/admin/contact/edit',{
-            list:result[0]
-        })
-    }
-    //提交数据
-    async doEdit() {
-        let result = this.ctx.request.body;
-        let id = result.id;
-        let concatResult = await this.ctx.model.Contact.updateOne({'_id':id},result);
-        await this.success('/admin/contact','修改联系人成功');
-    }
+
+   
     
 }
 
-module.exports = ContactController
+module.exports = UserController
