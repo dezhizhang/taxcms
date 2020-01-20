14a7ec0f04848cff111daae9979cc9d8f678d980
dezhizhang <1541609448@qq.com>
2020-01-20 20:00:48 +0800

    修改

--- config/config.default.js
+++ config/config.default.js
@@ -43,8 +43,8 @@ module.exports = appInfo => {
   config.cluster = {
     listen: {
       path: '',
-      port: 80,
-      hostname: '0.0.0.0',
+      port: 7001,
+      hostname: 'localhost',
     }
 };
 
