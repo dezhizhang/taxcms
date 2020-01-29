fddf4d0feffa5274982eb32be4ddd6031a6a7a63
dezhizhang <1541609448@qq.com>
2020-01-29 17:11:14 +0800

    提交

--- app/model/user.js
+++ app/model/user.js
@@ -2,10 +2,15 @@
 module.exports = app => {
     const mongoose = app.mongoose;
     const Schema = mongoose.Schema;
-
+    const d = new Date();
     const UserSchema = new Schema({
-        username:String,
-        password:String,
+        password:{ type:String },
+        phone:{ type:String },
+        add_time:{
+            type:Number,
+            default:d.getTime()
+        }
+
     });
     return mongoose.model('User',UserSchema,'user');
 }
